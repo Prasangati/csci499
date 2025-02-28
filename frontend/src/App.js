88f70829d01a7177cb66c085b7062dbc1d04fe20
Malak235 <158207977+Malak235@users.noreply.github.com>
2025-02-28 13:24:16 -0500

    Fix import path after file rename in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./pages/Login";
 import Journal from "./pages/Journal";
 import Login from "./pages/Dashboard"; 
 
