a3529d0103aa16aa42cb124ff59c1939f7b63803
Raitah Jinnat <raitahjinnat@gmail.com>
2025-03-07 00:47:59 -0500

    html barebones added

--- frontend/src/pages/EditProfile.js
+++ frontend/src/pages/EditProfile.js
@@ -4,3 +4,11 @@ import { useNavigate } from "react-router-dom";
 import "../App.css";
 import "../EditProfile.css";
 
+const EditProfile = () => {
+    const navigate = useNavigate();
+    return (
+        <div className="edit-profile-container">
+            <h1>Edit Profile</h1>
+        </div>
+    );
+}
