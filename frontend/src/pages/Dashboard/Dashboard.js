cf9068b047cbb542392aac1740ae160bdc47764d
Raitah Jinnat <raitahjinnat@gmail.com>
2025-03-09 22:37:45 -0400

    began work with dashboard nav bar

--- frontend/src/pages/Dashboard/Dashboard.js
+++ frontend/src/pages/Dashboard/Dashboard.js
@@ -1,11 +1,29 @@
 import React from "react";
+import { Avatar } from "@/components/ui/avatar";
 
 
-function Home() {
-   return <h1> dashboard after logging to account </h1>;
+function Dashboard() {
+   // need to figure out how to fetch data from the backend based on the database being used
+   return (
    // features needed: nav bar, sections: journal entries, progress tracking, etc
    // nav bar needs to be able to switch bw the diff parts of our app
+   <div className="dashboard-container">
+      <h1>Dashboard After logging in</h1>
+      // nav bar
+      <nav className="nav-bar">
+         <div className="dashboard-buttons">
+            <Button variant ="outline">Journal</Button>
+            <Button variant="outline">Progress</Button>
+            <Button variant="outline">Resources</Button>
+         </div>
+      <Avatar className="user-profile" /> // need to click so user can edit profile/log out
+      </nav>
+      // How do I fetch data from the backend?
+
+
+   </div>
+       )
 }
 
 
-export default Home;
+export default Dashboard;
