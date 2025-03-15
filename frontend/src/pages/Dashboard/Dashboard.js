81eefabaca104bd0b78179a6002d5ba2a45b69ef
Malak235 <158207977+Malak235@users.noreply.github.com>
2025-03-15 15:39:30 -0400

    added navigation tabs to Dashboard for Journal, Progress, and Resources

--- frontend/src/pages/Dashboard/Dashboard.js
+++ frontend/src/pages/Dashboard/Dashboard.js
@@ -13,13 +13,39 @@ function Dashboard() {
    // features needed: nav bar, sections: journal entries, progress tracking, etc
    // nav bar needs to be able to switch bw the diff parts of our app
    <div className="dashboard-container">
+
  {/* nav bar */}
+
       <nav className="nav-bar">
-        <div className="dashboard-buttons">
-            <button className="outline-button">Journal</button>
-            <button className="outline-button">Progress</button>
-            <button className="outline-button">Resources</button>
-        </div>
+      <h1 className="dashboard-heading">Dashboard</h1>
+
+      <div className="tabs">
+      <button 
+               className={`tab-button ${activeTab === "Journal" ? "active" : ""}`} 
+               onClick={() => setActiveTab("Journal")}
+           >
+               Journal
+           </button>
+           <button 
+                       className={`tab-button ${activeTab === "Progress" ? "active" : ""}`} 
+                       onClick={() => setActiveTab("Progress")}
+                   >
+                       Progress
+                   </button>
+                   <button 
+                       className={`tab-button ${activeTab === "Resources" ? "active" : ""}`} 
+                       onClick={() => setActiveTab("Resources")}
+                   >
+                       Resources
+                   </button>
+           </div>
+
+
+
+            {/*</div><button className="outline-button">Journal</button>*/}
+            {/*<button className="outline-button">Progress</button>*/}
+            {/*<button className="outline-button">Resources</button>*/}
+   
         <img 
             src="https://via.placeholder.com/50" 
             alt="User Profile" 
@@ -27,9 +53,16 @@ function Dashboard() {
         />
         {/* User can click this image to edit profile/log out */}
     </nav>
-</div>
-       )
-}
+         {/* tabs*/}
+         <div className="dashboard-content">
+                {activeTab === "Journal" && <p>Journal entries</p>}
+                {activeTab === "Progress" && <p>Track your progress here</p>}
+                {activeTab === "Resources" && <p> personalized mental health resources</p>}
+     
+               </div>
+               </div>
 
+       );
 
+      }
 export default Dashboard;
