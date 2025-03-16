cc649c5d872d18023310589efb0b6c1cc9723b26
Raitah Jinnat <raitahjinnat@gmail.com>
2025-03-16 14:30:07 -0400

    began implementing profile picture

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -14,9 +14,17 @@ export default function Home() {
   if (isAuthenticated) {
     return (
       <div>
-        <h1>Welcome, {user?.first_name || "User"}!</h1>
-        <LogOut />
-        {/* Additional authenticated content */}
+        <h1>Welcome, {user?.first_name || "User"} {user?.last_name || ""}!</h1>
+          { /* Profile Information */ }
+        <div className="profile-info">
+            <div className="profile-pic">
+                {/* Profile Pic: How to get it from the google account? */}
+                {/*/* How to change these components? */}
+                <img src={user?.profile_picture || '/default-avatar.png'} alt="Profile picture"/>
+            </div>
+        </div>
+          <LogOut/>
+          {/* Additional authenticated content */}
       </div>
     );
   } else {
